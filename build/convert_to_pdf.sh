07ad212edef40afad701b83e1c5e9c2ade89ee8b
Daniel Jeller <djeller@nettek.at>
2020-04-16 12:29:47 +0000

    try to fix pandoc build

--- build/convert_to_pdf.sh
+++ build/convert_to_pdf.sh
@@ -5,7 +5,9 @@ outpath=/opt/output
 
 find $inpath -name '*.md' -print0 | 
     while IFS= read -r -d '' file; do
+        cd $(dirname "$file") 
         name=$(basename "$file" .md)
-        sed -i "s/-release-version-/Release $VERSION - $(date '+%Y-%m-%d')/" $file
-        pandoc -s -F pandoc-mermaid --resource-path=$(dirname "$file") -o "$outpath"/"$name".pdf $file
+        sed -i "s/-release-version-/$VERSION - $(date '+%Y-%m-%d')/" $file
+        pandoc -s -F pandoc-mermaid -i "$name".md -o "$name".pdf $file
+        mv "$name".pdf $outpath
     done
