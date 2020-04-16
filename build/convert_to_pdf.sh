99bff04481c88f4d2f8e20ea0adbbfc2df47d284
Daniel Jeller <djeller@nettek.at>
2020-04-16 12:13:41 +0000

    Update build actions

--- build/convert_to_pdf.sh
+++ build/convert_to_pdf.sh
@@ -7,5 +7,5 @@ find $inpath -name '*.md' -print0 |
     while IFS= read -r -d '' file; do
         name=$(basename "$file" .md)
         sed -i "s/-release-version-/Release $VERSION - $(date '+%Y-%m-%d')/" $file
-        pandoc -s --resource-path=$(dirname "$file") -o "$outpath"/"$name".pdf $file
+        pandoc -s -F pandoc-mermaid --resource-path=$(dirname "$file") -o "$outpath"/"$name".pdf $file
     done
