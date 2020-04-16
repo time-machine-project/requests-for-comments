8918248163020699a489d5a889dfce36d5f666b0
Daniel Jeller <djeller@nettek.at>
2020-04-16 12:49:30 +0000

    Debug message

--- build/convert_to_pdf.sh
+++ build/convert_to_pdf.sh
@@ -7,6 +7,7 @@ find $inpath -name '*.md' -print0 |
     while IFS= read -r -d '' file; do
         cd $(dirname "$file") 
         name=$(basename "$file" .md)
+        echo $VERSION
         sed -i "s/-release-version-/$VERSION - $(date '+%Y-%m-%d')/" $file
         pandoc -s -F pandoc-mermaid -i "$name".md -o "$name".pdf
         mv "$name".pdf $outpath
