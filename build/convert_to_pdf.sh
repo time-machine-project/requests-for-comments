470b0c5fdb8c1375ab8726c86b453cb9945e4be2
Daniel Jeller <djeller@nettek.at>
2020-08-10 16:27:34 +0200

    Improve build and workflows
    Improve all GitHub actions and the necessary Dockerfile

--- build/convert_to_pdf.sh
+++ build/convert_to_pdf.sh
@@ -2,13 +2,17 @@
 
 inpath=/opt/input
 outpath=/opt/output
+version_text=${VERSION:=custom-build}
 
-find $inpath -name '*.md' -print0 | 
+find $inpath -name '*.md' -print0 |
     while IFS= read -r -d '' file; do
-        cd $(dirname "$file") 
+        cd $(dirname "$file")
+        mkdir -p "$outpath"/build
+        cp -R * "$outpath"/build
+        cd "$outpath"/build
         name=$(basename "$file" .md)
-        echo $VERSION
-        sed -i "s/-release-version-/$VERSION - $(date '+%Y-%m-%d')/" $file
-        pandoc -s -F pandoc-mermaid -i "$name".md -o "$name".pdf
-        mv "$name".pdf $outpath
+        echo "building: $version_text $name"
+        sed -i "s/-release-version-/$version_text - $(date '+%Y-%m-%d')/" ./"$name".md
+        pandoc -s -F pandoc-mermaid -i ./"$name".md -o ../"$name".pdf
+        rm -Rf "$outpath"/build
     done
