74254d5c814794c81b0b35443e7b99e1ae8dd0fc
Daniel Jeller <djeller@nettek.at>
2021-11-05 14:10:50 +0100

    Update conversion and build to remove env and mermaid

--- build/convert_to_pdf.sh
+++ build/convert_to_pdf.sh
@@ -5,14 +5,14 @@ outpath=/opt/output
 version_text=${VERSION:=custom-build}
 
 find $inpath -name '*.md' -print0 |
-    while IFS= read -r -d '' file; do
-        cd $(dirname "$file")
-        mkdir -p "$outpath"/build
-        cp -R * "$outpath"/build
-        cd "$outpath"/build
-        name=$(basename "$file" .md)
-        echo "building: $version_text $name"
-        sed -i "s/-release-version-/$version_text - $(date '+%Y-%m-%d')/" ./"$name".md
-        pandoc -s -F pandoc-mermaid -i ./"$name".md -o ../"$name".pdf
-        rm -Rf "$outpath"/build
-    done
+	while IFS= read -r -d '' file; do
+		"cd $(dirname "$file")"
+		mkdir -p "$outpath"/build
+		cp -R ./* "$outpath"/build
+		cd "$outpath"/build || exit
+		name=$(basename "$file" .md)
+		echo "building: $version_text $name"
+		sed -i "s/-release-version-/$version_text - $(date '+%Y-%m-%d')/" ./"$name".md
+		pandoc -s -i ./"$name".md -o ../"$name".pdf
+		rm -Rf "$outpath"/build
+	done
