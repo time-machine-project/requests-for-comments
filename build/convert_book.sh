46f660f7b259c2c4a3ed4e79d9ccfc06f150f0f8
Daniel Jeller <yngwi@users.noreply.github.com>
2025-03-24 14:38:17 +0100

    Update build process and unicode handling (#126)
    * set XeLaTeX as pandoc engine for build actions
    
    * Update actions
    
    * Change fonts for better unicode support
    
    * Add dejavu font

--- build/convert_book.sh
+++ build/convert_book.sh
@@ -71,7 +71,7 @@ for md in $(find . -type f -name "*.md" -not -name "RFC-template.md" -not -name 
 done
 
 # Build pdf
-pandoc -s -M date="Release $version - $(date "+%B %e, %Y")" --toc -f markdown -o "$pdfpath" <(echo "$text")
+pandoc -s --pdf-engine=xelatex -V mainfont="DejaVu Serif" -V sansfont="DejaVu Sans" -V monofont="DejaVu Sans Mono" -M date="Release $version - $(date "+%B %e, %Y")" --toc -f markdown -o "$pdfpath" <(echo "$text")
 
 # Remove temporary files
 cd ..
